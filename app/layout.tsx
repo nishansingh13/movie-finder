262f7e63745a1ce928dc7627c71054514d485308
Nishan Singh <nishansingh2480@gmail.com>
2025-02-02 14:14:40 +0530

    added some icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,11 +15,11 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Find movies streaming",
-  description: "Find out where is your favourite movies streaming which country and which platform",
+  description: "Find out where your favorite movies are streaming, which country, and which platform.",
   other: {
     "google-site-verification": "Jc3O9XhPMKz8PUkRtR5MG7t4Z-H1P82mF4kJq1cUo0E",
-    "google-adsense-account":"ca-pub-2845318690222180" // Replace with your full verification code
-  }
+    "google-adsense-account": "ca-pub-2845318690222180", // Replace with your full verification code
+  },
 };
 
 export default function RootLayout({
@@ -28,12 +29,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="application-name" content="MovieFinder" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-      
-          <main className="">{children}</main>
-      
+        <main className="">{children}</main>
       </body>
     </html>
   );
 }
-
