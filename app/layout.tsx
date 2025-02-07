e1e8d92e00930e550c8f167887cc499d518216d7
Nishan Singh <nishansingh2480@gmail.com>
2025-02-07 22:13:20 +0530

    trying logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Where To Watch – Find Streaming Availability Worldwide",
   description: "Want to know where to watch your favorite movie? Our platform helps you find streaming availability in your country and worldwide. Discover which platforms are streaming movies near you and where they are available if not in your region. Search now and never miss out on a great film!",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
   other: {
     "google-site-verification": "Jc3O9XhPMKz8PUkRtR5MG7t4Z-H1P82mF4kJq1cUo0E",
     "google-adsense-account": "ca-pub-2845318690222180", // Replace with your full verification code
